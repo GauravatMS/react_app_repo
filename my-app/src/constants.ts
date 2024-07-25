b63de0ddb15fb2d963c1ecd537c9fb5c0f44d54e
Gaurav Singh (TATA CONSULTANCY SERVICES LTD) <v-gauravsin@microsoft.com>
2024-07-25 14:05:30 +0000

    added REACT_APP_ in env rav name

--- my-app/src/constants.ts
+++ my-app/src/constants.ts
@@ -1,4 +1,4 @@
 export const SCRET_FROM_URL = process.env.REACT_APP_SCRET_FROM_URL;
 export const SECRET_FROM_IDENTIFIER = process.env.REACT_APP_SECRET_FROM_IDENTIFIER;
 export const ALT_SECRET = process.env.REACT_APP_ALT_SECRET;
-export const DEMO_ENV = process.env.DEMO_ENV;
+export const DEMO_ENV = process.env.REACT_APP_DEMO_ENV;
